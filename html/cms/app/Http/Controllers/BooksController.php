d8e779bf6807e3123435768b6b8599049f22249a
hayato-nima <kdc98bac72xkx@gmail.com>
2021-09-06 22:21:30 +0900

    add:クラスの読み込み

--- html/cms/app/Http/Controllers/BooksController.php
+++ html/cms/app/Http/Controllers/BooksController.php
@@ -1,8 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+//使うClassを宣言:自分で追加
+use App\Book;   //Bookモデルを使えるようにする
+use Validator;  //バリデーションを使えるようにする
+use Auth;       //認証モデルを使用する
+
 
 class BooksController extends Controller
 {
